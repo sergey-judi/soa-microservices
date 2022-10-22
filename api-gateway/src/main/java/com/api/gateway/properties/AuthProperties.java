09d654e8bd06e23be0f16e95e0ddb2c193c9624f
sergey-judi <serega7qwerty@gmail.com>
2022-10-22 11:07:54 +0300

    Added auth properties constraints

--- api-gateway/src/main/java/com/api/gateway/properties/AuthProperties.java
+++ api-gateway/src/main/java/com/api/gateway/properties/AuthProperties.java
@@ -2,14 +2,25 @@ package com.api.gateway.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
+@Validated
 @ConfigurationProperties(prefix = "api.auth")
 public class AuthProperties {
 
+  @NotBlank
   private String username;
+
+  @NotBlank
   private String password;
+
+  @NotBlank
   private String role;
+
+  @NotBlank
   private String protectedMapping;
 
 }
